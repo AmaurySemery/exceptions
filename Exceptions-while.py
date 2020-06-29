6fcd0f8e778f9692c10a36bd8d411a8b470673a9
Amaury Semery <amaury.semery@laposte.net>
2020-06-29 11:02:14 +0200

    Changement des noms

--- Exceptions-while.py
+++ Exceptions-while.py
@@ -1,18 +1,18 @@
 # year <- saisir une année
 # input ...
-bonrep = False
+b_bonrep = False
 
-while bonrep == False:
-    year = input("Date ? >")
+while b_bonrep == False:
+    s_year = input("Date ? >")
     # year est un string / chaîne de caractères
     # on test si year vaut 2000
     # '2000' =/= 2000
-    if year == "2000":
+    if s_year == "2000":
         print("Bien joué !")
     try:
-        yearnew=int(year)+10
+        i_yearnew=int(year)+10
         print(yearnew)
-        bonrep= True
+        b_bonrep= True
     except ZeroDivisionError:
         print("Division par 0 !")
     except Exception as error:
